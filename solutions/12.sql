7bc94908cd82f0e4124bc049691ab9d37999b7f2
SongyangZhou <zhousongyang_2023@163.com>
2026-04-15 11:50:19 -0400

    Modify JOINs to LEFT JOINs for song count query
    Changed JOINs to LEFT JOINs to include bands with no songs.

--- solutions/12.sql
+++ solutions/12.sql
@@ -2,6 +2,10 @@ SELECT
   bands.name AS 'Band',
   COUNT(songs.id) AS 'Number of Songs'
 FROM bands
-JOIN albums ON bands.id = albums.band_id
-JOIN songs ON albums.id = songs.album_id
+LEFT JOIN albums ON bands.id = albums.band_id
+LEFT JOIN songs ON albums.id = songs.album_id
 GROUP BY albums.band_id;
+
+/* 
+This will include the Dream Theater with 0 Number of Songs.
+*/
